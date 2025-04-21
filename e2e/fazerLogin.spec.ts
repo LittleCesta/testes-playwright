4a4278b75db1855ccd3c0035d6e713c91424e6e9
Cesar Romero <cesarromero34@gmail.com>
2025-04-21 16:55:32 -0300

    feat: implementação da fixture com importação personalizada

--- e2e/fazerLogin.spec.ts
+++ e2e/fazerLogin.spec.ts
@@ -1,30 +1,22 @@
-import test from "@playwright/test";
-import PaginaLogin from "./page-objects/PaginaLogin";
+import { test } from "./page-objects/PaginaLogin";
 
 test.describe("Página de login", () => {
-    test("Deve conseguir fazer login com email e senha válidos", async ({ page }) => {
-        const paginaLogin = new PaginaLogin(page);
-        await paginaLogin.visitar();
+    test("Deve conseguir fazer login com email e senha válidos", async ({ paginaLogin }) => {
         await paginaLogin.fazerLogin('cesarromero34@gmail.com', '12345678');
         await paginaLogin.loginFeitoComSucesso();
     });
 
-    test("Não deve conseguir fazer login com email invalido", async ({ page }) => {
-        const paginaLogin = new PaginaLogin(page);
-
-        await paginaLogin.visitar();
+    test("Não deve conseguir fazer login com email invalido", async ({ paginaLogin }) => {
         await paginaLogin.fazerLogin('errado_cesarromero34@gmail.com', '12345678');
         await paginaLogin.estaMostrandoMensagemDeErro('Você não está autorizado a acessar este recurso');
     });
 
-    test("Não deve conseguir fazer login com email incompleto", async ({ page }) => {
-        const paginaLogin = new PaginaLogin(page);
+    test("Não deve conseguir fazer login com email incompleto", async ({ paginaLogin }) => {
         await paginaLogin.visitar();
         await paginaLogin.estaMostrandoEmailInvalido('errado_cesarromero34gmail.com', '12345678', 'E-mail inválido');
     });
 
-    test("Não deve conseguir fazer login com campo em branco", async ({ page }) => {
-        const paginaLogin = new PaginaLogin(page);
+    test("Não deve conseguir fazer login com campo em branco", async ({ paginaLogin }) => {
         await paginaLogin.visitar();
         await paginaLogin.estaMostrandoEmailInvalido('', '12345678', 'E-mail é obrigatório');
     });
